f01d36e1553c59b31867f5125a71e4a523e947ec
Joshua Martius <Joshua.Martius@RWTH-Aachen.de>
2020-08-28 15:27:06 +0200

    Fixed notokenfound exception handling

--- runner.py
+++ runner.py
@@ -1,11 +1,15 @@
 import comie
 
 token = ""
-with open(".token","r") as file:
-    token = file.readline()
-    if token == "":
-        print("No token given. Exiting.")
-        exit()
-
+try:
+    with open(".token","r") as file:
+        token = file.readline()
+        if token == "":
+            print("No token given. Exiting.")
+            exit()
+except:
+    print("No .token file found. Exiting.")
+    exit()
+    
 bot = comie.Comie()
 bot.run(token)
