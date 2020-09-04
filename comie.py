83aac596c09c2865e005297f84a7652f2af714a9
Joshua Martius <Joshua.Martius@RWTH-Aachen.de>
2020-09-04 16:55:26 +0200

    Separated functions into multiple files

--- comie.py
+++ comie.py
@@ -1,14 +1,17 @@
 import discord
 import random
 from discord import utils
+from secretsanta import secretsanta
+
 adminNames = ["y0sh1#1990", "Sh4ky#3017"]
-santaRoleName = "Wichtel"
 
 def mentionUser(user):
     return "<@" + str(user.id) + ">"
 
 class Comie(discord.Client):
+    santaRoleName = "Wichtel"
 
+    ### READY MESSAGE
     async def on_ready(self):
         print("Bot is up and running.")
         return
@@ -19,48 +22,9 @@ class Comie(discord.Client):
 
         ##### SECRET SANTA
         if message.content.startswith("!wichteln") and str(message.author) in adminNames:
-            debug = message.content.endswith("debug")
-            try:
-                users = message.guild.members
-            except:
-                await message.author.send("Wichteln kann nicht aus einem privaten Chat heraus gestartet werden!")
-                return
-            role = utils.get(message.guild.roles, name=santaRoleName)
-            santas = []
-            for u in users:
-                if role in u.roles:
-                    santas.append(u)
-            
-            if len(santas) < 3:
-                await message.channel.send("Mindestens drei Leute müssen die Rolle \"" + santaRoleName + "\" besitzen... :/")
-                return
+            await secretsanta.exec(self, message)
 
-            await message.channel.send("Los gehts! %s Leute nehmen Teil!" % (len(santas)))
-            
-            shuffles = random.randint(1,10)
-            for i in range(shuffles):
-                random.shuffle(santas) # randomize the list
-            
-            if not debug:
-                for i in range(len(santas) - 1):
-                    santaFrom = santas[i]
-                    santaTo = str(santas[i + 1])
-                    await santaFrom.send("Dein Wichtelpartner ist: " + santaTo)
-
-                # Special message for the last entry
-                await santas[-1].send("Dein Wichtelpartner ist: " + str(santas[0]))
-            else:
-                for i in range(len(santas) - 1):
-                    santaFrom = str(santas[i])
-                    santaTo = str(santas[i + 1])
-                    await message.channel.send("%s hat %s bekommen." % (santaFrom, santaTo))
-                await message.channel.send("%s hat %s bekommen." % (str(santas[-1]), str(santas[0])))
-
-            await message.channel.send("Ich habe die Teilnehmerliste %d mal durchgewürfelt :)" % (shuffles))
-            await message.channel.send("Partnervergabe abgeschlossen! Viel Spaß!")
-            return
-        #####
-
+        ##### SELF HELP
         elif message.content.startswith("!help"):
             await message.channel.send("Hi " + mentionUser(message.author) + "!\nLeider kann ich noch nichts :(")
             return
---
+++ secretsanta.py
@@ -0,0 +1,49 @@
+
+from discord import utils
+import random
+
+class secretsanta():
+    async def exec(self, message):
+        debug = message.content.endswith("debug")
+        try:
+            users = message.guild.members
+        except:
+            await message.author.send("Wichteln kann nicht aus einem privaten Chat heraus gestartet werden!")
+            return
+
+        role = utils.get(message.guild.roles, name=self.santaRoleName)
+        santas = []
+        for u in users:
+            if role in u.roles:
+                santas.append(u)
+        
+        if len(santas) < 3:
+            await message.channel.send("Mindestens drei Leute müssen die Rolle \"" + self.santaRoleName + "\" besitzen... :/")
+            return
+
+        await message.channel.send("Los gehts! %s Leute nehmen Teil!" % (len(santas)))
+        
+        random.seed(random.randint(1, 1000))
+        shuffles = random.randint(1,10)
+        for i in range(shuffles):
+            random.shuffle(santas) # randomize the list
+        
+        if not debug:
+            for i in range(len(santas) - 1):
+                santaFrom = santas[i]
+                santaTo = str(santas[i + 1])
+                await santaFrom.send("Dein Wichtelpartner ist: " + santaTo)
+
+            # Special message for the last entry
+            await santas[-1].send("Dein Wichtelpartner ist: " + str(santas[0]))
+        else:
+            for i in range(len(santas) - 1):
+                santaFrom = str(santas[i])
+                santaTo = str(santas[i + 1])
+                await message.channel.send("%s hat %s bekommen." % (santaFrom, santaTo))
+            await message.channel.send("%s hat %s bekommen." % (str(santas[-1]), str(santas[0])))
+
+        await message.channel.send("Ich habe die Teilnehmerliste %d mal durchgewürfelt :)" % (shuffles))
+        await message.channel.send("Partnervergabe abgeschlossen! Viel Spaß %ss!" % (self.santaRoleName))
+        return
+    
