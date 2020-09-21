33662c1ff8b9666673d1325b117ac28cd8acd183
Joshua Martius <Joshua.Martius@RWTH-Aachen.de>
2020-09-21 21:07:34 +0200

    Added basic voting

--- comie.py
+++ comie.py
@@ -12,25 +12,45 @@ def mentionUser(user):
 class Comie(discord.Client):
     santaRoleName = "Wichtel"
 
+    ### REACIONS
+    async def on_reaction_add(self, reaction, user):
+        if str(reaction.message.author) != "Comie#1396":
+            return
+        if reaction.emoji != "👍":
+            return
+        
+        await imgur.reaction(self, reaction, user)
+        return
+
     ### READY MESSAGE
     async def on_ready(self):
         print("Bot is up and running.")
         return
     
     async def on_message(self, message):
-        if message.author == self.user:
+        if message.author == self.user: 
             return
 
         ##### SECRET SANTA
         if message.content.startswith("!wichteln") and str(message.author) in adminNames:
             await secretsanta.exec(self, message)
+            return
 
         ##### IMGUR
         elif message.content.startswith("!img"):
             await message.channel.send("Hier kommt ein zufälliges Bild für dich %s ~(^__^)~" % (mentionUser(message.author)))
-            await imgur.exec(self, message)
+            await imgur.postImage(self, message, message.author)
             return
 
+        elif message.content.startswith("!results"):
+            results = await imgur.postResults(self, message.channel)
+            winmsg = await message.channel.fetch_message(results[0])
+            winner = await self.fetch_user(results[2])
+            
+            await message.channel.send("Gewonnen hat %s mit %d Votes für das gepinnte Bild!" % (mentionUser(winner), int(results[1])))
+            await winmsg.pin()
+            return
+        
         ##### SELF HELP
         elif message.content.startswith("!help"):
             await message.channel.send("Hi " + mentionUser(message.author) + "!\nIch kann folgende Befehle bearbeiten:\n!help - Zeigt diese Hilfe an\n!img - Schickt ein zufälliges Bild in den aktuellen Channel\n!wichteln - Startet eine Wichtelpaar Auslosung")
--- imgur.py
+++ imgur.py
@@ -5,8 +5,13 @@ import string
 import urllib.request
 from PIL import Image
 
-class imgur():    
-    async def exec(self, message):
+imageIDs = []
+authorIDs = []
+votes = []
+
+class imgur():
+
+    async def postImage(self, message, author):
         baseString = "https://i.imgur.com"
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         len = random.randint(5,6)
@@ -17,8 +22,23 @@ class imgur():
 
         with Image.open("img.png") as img:
             if img.width == 161 and img.height == 81: # in case of the imgur error image
-                await imgur.exec(self, message)
+                await imgur.postImage(self, message, author)
                 return
 
-        await message.channel.send(file=discord.File('img.png'))
+        message = await message.channel.send(file=discord.File('img.png'))
+
+        imageIDs.append(message.id)
+        authorIDs.append(author.id)
+        votes.append(0)
+        return
+
+    async def postResults(self, channel):
+        winnerIndex = votes.index(max(votes))
+        return [imageIDs[winnerIndex], votes[winnerIndex], authorIDs[winnerIndex]]
+
+    async def reaction(self, reaction, user):
+        imgid = reaction.message.id
+        index = imageIDs.index(imgid)
+
+        votes[index] = votes[index] + 1
         return
