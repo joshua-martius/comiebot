26e7e51b3ddf95fce0302a7c5a80239df748c905
Joshua Martius <Joshua.Martius@RWTH-Aachen.de>
2020-09-29 13:12:42 +0200

    Expanded the imgur universe

--- imgur.py
+++ imgur.py
@@ -8,6 +8,7 @@ from PIL import Image
 imageIDs = []
 authorIDs = []
 votes = []
+images = []
 
 def mentionUser(user):
     return "<@" + str(user.id) + ">"
@@ -17,16 +18,26 @@ class imgur():
     async def postImage(self, message, author):
         baseString = "https://i.imgur.com"
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        len = random.randint(5,6)
-        id = ''.join(random.choice(chars) for i in range(5))
-        url = "%s/%s.png" % (baseString, id)
-        
-        urllib.request.urlretrieve(url, './img.png')
-
-        with Image.open("img.png") as img:
-            if img.width == 161 and img.height == 81: # in case of the imgur error image
-                await imgur.postImage(self, message, author)
+        ### generate unique id
+        tries = 0
+        while True:
+            length = random.randint(5,6)
+            tries = tries + 1
+            if tries >= 100:
+                await message.channel.send("Ich habe nach %d Versuchen kein neues Bild finden können </3" % (tries))
                 return
+            id = ''.join(random.choice(chars) for i in range(length))
+            url = "%s/%s.png" % (baseString, id)
+            
+            try:
+                urllib.request.urlretrieve(url, './img.png')
+                with Image.open("img.png") as img:
+                    if img.width != 161 and img.height != 81 and id not in images: # in case of the imgur error image
+                        break
+            except:
+                continue
+        ### when unique id was found
+        images.append(id)
 
         message = await message.channel.send(file=discord.File('img.png'))
 
