5ea233efb4dc0c5d9a0c43c4ed3deaaf013bab14
y0sh1DE <Joshua.Martius@RWTH-Aachen.de>
2021-02-24 22:54:20 +0100

    Fixed rolehandler

--- rolehandler.py
+++ rolehandler.py
@@ -2,10 +2,10 @@ import discord
 import pymysql
 from discord import utils
 
-
 class rolehandler():
     async def init(self):
-        message = await self.guilds[0].channels[-1].fetch_message(pymysql.config["roles"]["reactionMessage"])
+        # ToDo: search channel instead of hardcoding it
+        message = await self.guilds[0].channels[-2].fetch_message(pymysql.config["roles"]["reactionMessage"])
         for key, value in pymysql.config["roles"]["list"].items():
             await message.add_reaction(value)
         print("Initialized rolehandler.")
@@ -13,28 +13,32 @@ class rolehandler():
 
     async def reactionAdded(self, userid, emojiname, messageid):
         # find role associated to emoji
-        role = ""
-        message = await self.guilds[0].channels[-1].fetch_message(pymysql.config["roles"]["reactionMessage"])
+        message = await self.guilds[0].channels[-2].fetch_message(pymysql.config["roles"]["reactionMessage"])
         for key, value in pymysql.config["roles"]["list"].items():
             if value == str(emojiname):
                 role = utils.get(message.guild.roles, name=key)
                 break
 
         # give user the found role
-        user = utils.get(self.get_all_members(), id=userid)
-        await user.add_roles(role)
+        try:
+            user = utils.get(self.get_all_members(), id=userid)
+            await user.add_roles(role)
+        except:
+            print("Tried to give role that doesnt exist.")
         return
 
     async def reactionRemoved(self, userid, emojiname, messageid):
         # find role associated to emoji
-        role = ""
-        message = await self.guilds[0].channels[-1].fetch_message(pymysql.config["roles"]["reactionMessage"])
+        message = await self.guilds[0].channels[-2].fetch_message(pymysql.config["roles"]["reactionMessage"])
         for key, value in pymysql.config["roles"]["list"].items():
             if value == str(emojiname):
                 role = utils.get(message.guild.roles, name=key)
                 break
                 
         # take user the found role
-        user = utils.get(self.get_all_members(), id=userid)
-        await user.remove_roles(role)
+        try:
+            user = utils.get(self.get_all_members(), id=userid)
+            await user.remove_roles(role)
+        except:
+            print("Tried to take role that doesnt exist.")
         return
