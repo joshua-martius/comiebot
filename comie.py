0b5e82963722076141435208cb618b78f9f042c7
y0sh1DE <Joshua.Martius@RWTH-Aachen.de>
2021-03-13 13:31:54 +0100

    fixed latest commit

--- comie.py
+++ comie.py
@@ -182,17 +182,17 @@ class Comie(discord.Client):
         ##### IMGUR
         elif command == "img":
             params = message.content.split(" ")
-            if len(params) == 0:
+            if len(params) == 1:
                 await message.channel.send("Hier kommt ein zufälliges Bild für dich %s ~(^__^)~" % (mentionUser(message.author)))
                 await imgur.postImage(self, message, message.author)
             else:
-                if params[1] == "top":
+                if params[-1] == "top":
                     cmd = "SELECT * FROM viewImages LIMIT 5"
                     result = pymysql.executeSql(cmd)
                     msg = "Die 5 größten Image-Spammer:\n"
                     for i in range(len(result)):
                         msg = msg + ("%d. %s - %d Images\n" % (i+1, result[i][0],result[i][2]))
-                await message.channel.send(msg)
+                    await message.channel.send(msg)
             return
 
         ##### IMGUR - RESULTS
