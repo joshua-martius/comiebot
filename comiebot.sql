4e5814406b1ba0be447a3f2dd01c363bcbb875f2
y0sh1DE <joshua.martius@rwth-aachen.de>
2021-04-07 01:14:41 +0200

    Revert "Update comiebot.sql"
    This reverts commit 91d5578d97af7d809282928f9823a6a9c4d7e6c7.

--- comiebot.sql
+++ comiebot.sql
@@ -2,47 +2,63 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE DATABASE IF NOT EXISTS `comiebot` DEFAULT CHARACTER SET utf16 COLLATE utf16_german2_ci;
+USE `comiebot`;
 
-CREATE DATABASE IF NOT EXISTS `serwmsound` DEFAULT CHARACTER SET utf16 COLLATE utf16_german2_ci;
-USE `serwmsound`;
-
-CREATE TABLE `tblComment` (
-  `cID` int(11) NOT NULL,
-  `cUploadID` varchar(16) COLLATE utf16_german2_ci NOT NULL,
-  `cAuthor` varchar(16) COLLATE utf16_german2_ci NOT NULL,
-  `cComment` text COLLATE utf16_german2_ci NOT NULL,
-  `cCreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cCreatedFrom` varchar(32) COLLATE utf16_german2_ci NOT NULL
+CREATE TABLE `tblAnime` (
+  `aID` int(11) NOT NULL,
+  `aTitle` varchar(128) COLLATE utf16_german2_ci NOT NULL,
+  `aLink` varchar(512) COLLATE utf16_german2_ci NOT NULL,
+  `aCreator` varchar(64) COLLATE utf16_german2_ci NOT NULL,
+  `aCreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `aTags` varchar(128) COLLATE utf16_german2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
-CREATE TABLE `tblUpload` (
-  `uID` varchar(32) COLLATE utf16_german2_ci NOT NULL,
-  `uUploaded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `uUploaderIP` varchar(32) COLLATE utf16_german2_ci NOT NULL,
-  `uInterpret` varchar(32) COLLATE utf16_german2_ci DEFAULT NULL,
-  `uName` varchar(64) COLLATE utf16_german2_ci DEFAULT NULL,
-  `uLyrics` text COLLATE utf16_german2_ci,
-  `uAllowComments` tinyint(1) NOT NULL DEFAULT '1',
-  `uHits` int(11) NOT NULL DEFAULT '0'
+CREATE TABLE `tblUser` (
+  `uName` varchar(64) COLLATE utf16_german2_ci NOT NULL,
+  `uID` varchar(64) COLLATE utf16_german2_ci NOT NULL,
+  `uChips` bigint(64) NOT NULL DEFAULT '1000',
+  `uCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
+CREATE TABLE `tblVoting` (
+  `vID` int(11) NOT NULL,
+  `vMessage` varchar(256) COLLATE utf16_german2_ci NOT NULL,
+  `vVotes` int(11) NOT NULL DEFAULT '0',
+  `vAuthor` varchar(64) COLLATE utf16_german2_ci NOT NULL,
+  `vCreated` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
+CREATE TABLE `viewImages` (
+`uName` varchar(64)
+,`uID` varchar(64)
+,`Images` bigint(21)
+);
+DROP TABLE IF EXISTS `viewImages`;
 
-ALTER TABLE `tblComment`
-  ADD PRIMARY KEY (`cID`),
-  ADD KEY `FK_uUpload` (`cUploadID`);
-
-ALTER TABLE `tblUpload`
-  ADD PRIMARY KEY (`uID`);
+CREATE ALGORITHM=UNDEFINED DEFINER=`comie`@`%` SQL SECURITY DEFINER VIEW `viewImages`  AS  select `tblUser`.`uName` AS `uName`,`tblUser`.`uID` AS `uID`,(select count(0) from `tblVoting` where (`tblVoting`.`vAuthor` = `tblUser`.`uID`)) AS `Images` from `tblUser` order by `Images` desc ;
 
 
-ALTER TABLE `tblComment`
-  MODIFY `cID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tblAnime`
+  ADD PRIMARY KEY (`aID`),
+  ADD KEY `FK_UID_UID` (`aCreator`);
+
+ALTER TABLE `tblUser`
+  ADD PRIMARY KEY (`uName`),
+  ADD UNIQUE KEY `FK_UNIQUE` (`uID`);
+
+ALTER TABLE `tblVoting`
+  ADD PRIMARY KEY (`vID`),
+  ADD UNIQUE KEY `vMessageID` (`vMessage`);
 
 
-ALTER TABLE `tblComment`
-  ADD CONSTRAINT `FK_uUpload` FOREIGN KEY (`cUploadID`) REFERENCES `tblUpload` (`uID`);
+ALTER TABLE `tblAnime`
+  MODIFY `aID` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `tblVoting`
+  MODIFY `vID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `tblAnime`
+  ADD CONSTRAINT `FK_UID_UID` FOREIGN KEY (`aCreator`) REFERENCES `tblUser` (`uID`);
 COMMIT;
+
